e7fcb00d68400cee95ab0eb160a82bb4a1989213
Benjamin Bevan <bbevan196@gmail.com>
2021-09-27 13:15:49 +0100

    add web mvc project

--- Bookish2.ConsoleApp/Program.cs
+++ Bookish2.ConsoleApp/Program.cs
@@ -9,19 +9,6 @@ namespace Bookish2.ConsoleApp
         {
             BookRepository br = new BookRepository();
 
-            br.AddBook("Tales to Give You Goosebumps", "Stine, R.L.", "0590134345", "barcodebarco3", 129, 3);
-
-            foreach (Book b in br.GetAllBooks())
-                Console.WriteLine(b);
-            foreach (Copy c in br.GetAllCopies())
-                Console.WriteLine(c);
-
-            //br.BorrowBook(1, "0747532745");
-            //br.BorrowBook(2, "074753274");
-
-            //foreach (Copy c in br.GetAllCopies())
-            //    Console.WriteLine(c);
-
             Console.WriteLine("\ndone");
             Console.ReadLine();
         }
