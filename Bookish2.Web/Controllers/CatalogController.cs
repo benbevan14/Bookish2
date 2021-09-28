c67a3e3ed62680b1ef70bcb8bf01ce09fec533ac
Benjamin Bevan <bbevan196@gmail.com>
2021-09-28 10:18:39 +0100

    books in proper controller and displayed as table

---
+++ Bookish2.Web/Controllers/CatalogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookish2.DataAccess;
+using Bookish2.Web.Models;
+
+namespace Bookish2.Web.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            BookRepository br = new BookRepository();
+            var books = br.GetAllBooks()
+                .Select(b => new BookModel()
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    ISBN = b.ISBN,
+                    Pages = b.Pages
+                })
+                .ToList();
+
+            return View(books);
+        }
+    }
+}
--- Bookish2.Web/Controllers/HomeController.cs
+++ Bookish2.Web/Controllers/HomeController.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Bookish2.DataAccess;
-using Bookish2.Web.Models;
 
 namespace Bookish2.Web.Controllers
 {
@@ -13,23 +11,6 @@ namespace Bookish2.Web.Controllers
         public ActionResult Index()
         {
             return View();
-        }
-
-        public ActionResult Books()
-        {
-            BookRepository br = new BookRepository();
-            var books = br.GetAllBooks()
-                .Select(b => new BookModel()
-                {
-                    Id = b.Id,
-                    Title = b.Title,
-                    Author = b.Author,
-                    ISBN = b.ISBN,
-                    Pages = b.Pages
-                })
-                .ToList();
-
-            return View(books);
         }
 
         public ActionResult About()
